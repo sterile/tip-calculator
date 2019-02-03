cf1a4cd2c898e2be142e066fe624dd0f5ecef209
Brandon Berney <brandonberney@protonmail.com>
2019-02-03 14:05:14 -0500

    docs: add comments to both files since idk where to actually put them!

--- tip-calculator/Form1.cs
+++ tip-calculator/Form1.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Grading ID: M5477
+ * Lab: 2
+ * Due Date: Feb 4 2019
+ * Course Section: 01
+ * Description: A GUI application that helps a user calculate a tip based on their input.
+ */
+ 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,7 +32,7 @@ namespace tip_calculator
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
--- tip-calculator/Program.cs
+++ tip-calculator/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Grading ID: M5477
+ * Lab: 2
+ * Due Date: Feb 4 2019
+ * Course Section: 01
+ * Description: A GUI application that helps a user calculate a tip based on their input.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
